efa2a3c880f1e27a8a19be350f70f21d9b7a16de
buxue.tm <buxue.tm@alibaba-inc.com>
2020-10-28 10:31:46 +0800

    a new web

---
+++ doc/precache-manifest.23261f694ed78b240d45043cff289999.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef2dc284e1ce824d5ab1800b890c950",
+    "url": "./index.html"
+  },
+  {
+    "revision": "850ce1fd18e305aa3413",
+    "url": "./static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "aeb6ebd1567da07b6a4a",
+    "url": "./static/css/main.016160dd.chunk.css"
+  },
+  {
+    "revision": "850ce1fd18e305aa3413",
+    "url": "./static/js/2.ab864491.chunk.js"
+  },
+  {
+    "revision": "542c91f071fe828f7759f188e3ae7231",
+    "url": "./static/js/2.ab864491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb6ebd1567da07b6a4a",
+    "url": "./static/js/main.6ee372bf.chunk.js"
+  },
+  {
+    "revision": "11eef22bcbd43090224c",
+    "url": "./static/js/runtime-main.e90bd347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
